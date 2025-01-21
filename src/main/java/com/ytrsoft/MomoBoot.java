484726514296818ee392b7d8f5f38294b4d8f33b
ytrsoft <1654486219@qq.com>
2025-01-21 11:33:42 +0800

    init

---
+++ src/main/java/com/ytrsoft/MomoBoot.java
@@ -0,0 +1,13 @@
+package com.ytrsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomoBoot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomoBoot.class, args);
+    }
+
+}
