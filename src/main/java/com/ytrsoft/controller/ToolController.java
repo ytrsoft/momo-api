d8ba058363c7b52f881446ca7586b0623cc81a5d
ytrsoft <1654486219@qq.com>
2025-01-21 12:56:48 +0800

    参数解析

--- src/main/java/com/ytrsoft/controller/ToolController.java
+++ src/main/java/com/ytrsoft/controller/ToolController.java
@@ -1,24 +1,34 @@
 package com.ytrsoft.controller;
 
 import com.ytrsoft.core.Props;
+import com.ytrsoft.service.ToolService;
+import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/tool")
 public class ToolController {
 
     private final Props props;
+    private final ToolService ts;
 
-    public ToolController(Props props) {
+    public ToolController(Props props, ToolService ts) {
         this.props = props;
+        this.ts = ts;
     }
 
     @GetMapping("/props")
     public ResponseEntity<Props> props() {
         return new ResponseEntity<>(props, HttpStatus.OK);
+    }
+
+    @PostMapping("/zip")
+    public ResponseEntity<Map<String, Object>> zip(@RequestParam("zip") String zip) {
+        Map<String, Object> args = ts.parseZip(zip);
+        return new ResponseEntity<>(args, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/ytrsoft/service/ToolService.java
@@ -0,0 +1,29 @@
+package com.ytrsoft.service;
+
+import com.ytrsoft.core.Base64;
+import com.ytrsoft.core.Coded;
+import com.ytrsoft.core.Props;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class ToolService {
+
+    private final Props props;
+
+    public ToolService(Props props) {
+        this.props = props;
+    }
+
+    public Map<String, Object> parseZip(String zip) {
+        byte[] key = props.getKey().getBytes();
+        byte[] data = Base64.decode(zip.getBytes());
+        byte[] decoded = Coded.decode(data, key);
+        String body = new String(decoded);
+        JSONObject jsonObject = new JSONObject(body);
+        return jsonObject.toMap();
+    }
+
+}
